128d82c41b09c3ec3b1a9b6afce48b8654cc8a69
Tal <Tal@Hope>
2014-11-10 12:25:56 +0200

    new

--- src/homeWork/tal/nemi/HomeWorkServlet.java
+++ src/homeWork/tal/nemi/HomeWorkServlet.java
@@ -8,6 +8,17 @@ public class HomeWorkServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
+		resp.getWriter().println("Hello, Tal");
+
+		int num1;
+		int num2;
+		int num3;
+		num1 = 4;
+		num2 = 3;
+		num3 = 7;
+		int result = (num1 + num2) * num3;
+		String resultStr = new String("<h1>Result of"+"( "+num1+"+"+num2+" )"+"*"+num3+"="+result+"</h1>");
+		resp.getWriter().println(resultStr);
+	
 	}
 }
