e49670bfb741eff7de17178299f8e0956e30e5b5
Tal <Tal@Hope>
2014-11-24 19:03:12 +0200

    new

--- src/homeWork/tal/nemi/HomeWorkServlet.java
+++ src/homeWork/tal/nemi/HomeWorkServlet.java
@@ -17,24 +17,24 @@ public class HomeWorkServlet extends HttpServlet {
 		num2 = 3;
 		num3 = 7;
 		int result = (num1 + num2) * num3;
-		String resultStr = new String("<h1>Result of"+"( "+num1+"+"+num2+" )"+"*"+num3+"="+result+"</h1>");
+		String resultStr = new String("Result of "+"( "+num1+"+"+num2+" )"+"*"+num3+"="+result+"<br/><br/>");
 		resp.setContentType("text/html");
 		resp.getWriter().println(resultStr);
 		
-		double radius = 50;
+		int radius = 50;
 		double area = Math.PI * Math.pow(radius, 2);
-		String line1 = new String ("<h1>Area of circle with radius "+(radius)+" is: "+(area)+" square-cm<h1>");
+		String line1 = new String ("Area of circle with radius "+(radius)+" is: "+(area)+" square-cm<br/>");
 		
 		int Hypotenuse = 50;
 		double opposite;
 		double angleB = Math.toRadians(30);
 		opposite = Math.sin(angleB) * Hypotenuse;
-		String line2 = new String("<h1>Length of opposite where angle B is 30 degrees and Hypotenuse length is 50 cm is: "+(opposite)+ "cm<h1>");
+		String line2 = new String("Length of opposite where angle B is 30 degrees and Hypotenuse length is 50 cm is: "+(opposite)+ "cm<br/>");
 
-		double base = 20;
-		double exp = 13;
+		int base = 20;
+		int exp = 13;
 		double resultPow = Math.pow(base, exp);
-		String line3 = new String("<h1>Power of 20 with exp of 13 is "+(resultPow)+"<h1>");
+		String line3 = new String("Power of "+base+ " with exp of "+exp+" is: "+(resultPow)+"<br/>");
 
 		String finalStr = (line1 + "<br>" + line2 + "<br>" +line3);
 		resp.getWriter().println(finalStr);
