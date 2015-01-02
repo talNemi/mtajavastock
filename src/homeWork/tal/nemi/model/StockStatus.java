e169f893c067c9d4a3f251c54f4344d9e5781915
Tal <Tal@Hope>
2015-01-02 21:01:46 +0200

    EX8

---
+++ src/homeWork/tal/nemi/model/StockStatus.java
@@ -0,0 +1,48 @@
+package homeWork.tal.nemi.model;
+
+import java.util.Date;
+
+import homeWork.tal.nemi.model.Portfolio.ALGO_RECOMMENDATION;
+
+/**
+ * instance of this class represents a stock
+ */
+public class StockStatus extends Stock {
+
+	private ALGO_RECOMMENDATION recommendation;
+	protected int stockQuantity;
+	
+	/**
+	 * This constructor is used to initialize members in StockStatus
+	 */
+	public StockStatus (){
+		
+		super();
+		recommendation=ALGO_RECOMMENDATION.DO_NOTHING;
+		stockQuantity=0;
+	}
+	/**
+	 * The copy constructor is used to duplicate an instance with all of his members.
+	 * @param stockStatus
+	 */
+	public StockStatus (StockStatus stockStatus){
+		
+		super(stockStatus);
+		recommendation=stockStatus.getRecommendation();
+		stockQuantity=stockStatus.getStockQuantity();
+	}
+	
+	public ALGO_RECOMMENDATION getRecommendation() {
+		return recommendation;
+	}
+	public int getStockQuantity() {
+		return stockQuantity;
+	}
+	public void setRecommendation(ALGO_RECOMMENDATION recommendation) {
+		this.recommendation = recommendation;
+	}
+	public void setStockQuantity(int stockQuantity) {
+		this.stockQuantity = stockQuantity;
+	}	
+	
+}
